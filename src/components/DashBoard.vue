5c142543650bcfb3bbb36784e8d610e0623221f5
Liza <liza@MacBook-Pro-Liza.local>
2022-07-06 10:28:38 +0200

    Styling chips

--- src/components/DashBoard.vue
+++ src/components/DashBoard.vue
@@ -17,8 +17,11 @@
             <div>{{ project.due }}</div>
           </v-flex>
           <v-flex xs2 sm4 md2>
-            <div class="caption grey--text">Status</div>
-            <div>{{ project.status }}</div>
+            <div>
+              <v-chip small :class="`${project.status} white--text caption my-2 ml-5`">
+                {{ project.status }}
+              </v-chip>
+            </div>
           </v-flex>
         </v-layout>
       </v-card>
@@ -83,5 +86,17 @@ export default defineComponent({
 
 .overdue {
   border-left: 4px solid tomato;
+}
+
+.v-chip.complete {
+  background: #3cd1c2 !important;
+}
+
+.v-chip.ongoing {
+  background: orange !important;
+}
+
+.v-chip.overdue {
+  background: tomato !important;
 }
 </style>
