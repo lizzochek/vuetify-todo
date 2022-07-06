fb69aa0fbb4a36647127d6ce7de50b485d593404
Liza <liza@MacBook-Pro-Liza.local>
2022-07-06 12:32:06 +0200

    Team page and avatar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -17,6 +17,14 @@
     </v-app-bar>
 
     <v-navigation-drawer v-model="drawer" app>
+      <v-layout column align-center justify-center>
+        <v-flex class="mt-5">
+          <v-avatar size="100">
+            <img src="avatar-1.png" alt="my avatar" />
+          </v-avatar>
+          <p class="subheading mt-1 text-center" center>Liza</p>
+        </v-flex>
+      </v-layout>
       <v-list class="bg-deep-purple">
         <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
           <v-list-item-action>
--- src/components/TeamComp.vue
+++ src/components/TeamComp.vue
@@ -1,7 +1,27 @@
 <template>
   <div class="team">
     <h1 class="subheading grey--text">Team</h1>
-    <v-container class="my-5"> content </v-container>
+    <v-container class="my-5">
+      <v-layout row wrap>
+        <v-flex xs12 sm6 md4 lg3 v-for="person in team" :key="person.name">
+          <v-card text class="text-center ma-3">
+            <v-responsive class="pt-4">
+              <v-avatar size="100" class="grey lighten-2 ml-5">
+                <img :src="person.avatar" alt="avatar" />
+              </v-avatar>
+            </v-responsive>
+            <v-card-text>
+              <div class="subheading">{{ person.name }}</div>
+              <div class="grey--text">{{ person.role }}</div></v-card-text
+            >
+            <v-card-actions text color="grey" class="justify-center">
+              <v-icon small left>message</v-icon>
+              <span class="text-uppercase grey--text">message</span></v-card-actions
+            >
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </div>
 </template>
 
@@ -10,5 +30,16 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'TeamComp',
+  data() {
+    return {
+      team: [
+        { name: 'Liza', role: 'Web developer', avatar: 'avatar-1.png' },
+        { name: 'Anna', role: 'Graphic designer', avatar: 'avatar-2.png' },
+        { name: 'Sam', role: 'Web developer', avatar: 'avatar-4.png' },
+        { name: 'John', role: 'Social media manager', avatar: 'avatar-5.png' },
+        { name: 'Marina', role: 'Sales manager', avatar: 'avatar-3.png' },
+      ],
+    };
+  },
 });
 </script>
